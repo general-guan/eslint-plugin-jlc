317b42b1f506b32c997d26616a3ce698a5319190
guanjunjiang <934750771@qq.com>
2024-08-24 15:03:04 +0800

    camelcase

---
+++ docs/rules/camelcase.md
@@ -0,0 +1,35 @@
+# 变量和函数、参数命名全部采用小写驼峰命名，代码中的命名开头结尾不允许使用下划线或符号 (`camelcase`)
+
+Please describe the origin of the rule here.
+
+## Rule Details
+
+This rule aims to...
+
+Examples of **incorrect** code for this rule:
+
+```js
+
+// fill me in
+
+```
+
+Examples of **correct** code for this rule:
+
+```js
+
+// fill me in
+
+```
+
+### Options
+
+If there are any options, describe them here. Otherwise, delete this section.
+
+## When Not To Use It
+
+Give a short description of when it would be appropriate to turn off this rule.
+
+## Further Reading
+
+If there are other links that describe the issue this rule addresses, please include them here in a bulleted list.
---
+++ tests/lib/rules/camelcase.js
@@ -0,0 +1,44 @@
+/**
+ * @fileoverview 变量和函数、参数命名全部采用小写驼峰命名，代码中的命名开头结尾不允许使用下划线或符号
+ * @author gjj
+ */
+"use strict";
+
+//------------------------------------------------------------------------------
+// Requirements
+//------------------------------------------------------------------------------
+
+const rule = require("../../../lib/rules/camelcase"),
+  RuleTester = require("eslint").RuleTester;
+
+//------------------------------------------------------------------------------
+// Tests
+//------------------------------------------------------------------------------
+
+const ruleTester = new RuleTester();
+ruleTester.run("camelcase", rule, {
+  valid: [
+    // give me some code that won't trigger a warning
+    "const loadingModules = {}",
+    "function stringFormat(source,typeName) {}",
+  ],
+
+  invalid: [
+    {
+      code: "const loading_modules = {}",
+      errors: [{ messageId: "camelcase", type: "Identifier" }],
+    },
+    {
+      code: "function string_format(source) {}",
+      errors: [{ messageId: "camelcase", type: "Identifier" }],
+    },
+    {
+      code: "function stringFormat(source,type_name) {}",
+      errors: [{ messageId: "camelcase", type: "Identifier" }],
+    },
+    {
+      code: "const LoadingModules = {}",
+      errors: [{ messageId: "camelcase", type: "Identifier" }],
+    },
+  ],
+});
